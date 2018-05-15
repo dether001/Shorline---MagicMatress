2eeb7c3eb42edf298107874831d5f061740e6595
Armandulis <Armandulis@gmail.com>
2018-05-15 13:45:09 +0200

    convertiondone

--- src/shoreline/Shoreline.java
+++ src/shoreline/Shoreline.java
@@ -19,7 +19,7 @@ public class Shoreline extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SelectCompany.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("NewPatternWindow.fxml"));
         
         Scene scene = new Scene(root);
         
