9846c644e5266b539301299835d8111a96471944
attilka19 <31649896+atilka19@users.noreply.github.com>
2018-05-28 15:06:57 +0200

    Merge done

--- src/shoreline/Shoreline.java
+++ src/shoreline/Shoreline.java
@@ -16,25 +16,24 @@ import javafx.stage.Stage;
  *
  * @author dell
  */
-public class Shoreline extends Application {
+public class Shoreline extends Application 
+{
     
     @Override
-    public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
+    public void start(Stage stage) throws Exception 
+    {
+        
         Font.loadFont(Shoreline.class.getResource("/shoreline/GUI/View/Images/Neuton-Extrabold.ttf").toExternalForm(), 10);
+        
         Parent root = FXMLLoader.load(getClass().getResource("/shoreline/GUI/View/SelectCompany.fxml"));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("/shoreline/GUI/View/LoginWindow.fxml"));
->>>>>>> 1dad1543b9c6b0c700402dcd5de0972700ac784c
-        
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.setTitle("ShoreLine - Data Converter");
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         launch(args);
     }
     
