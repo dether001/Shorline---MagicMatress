216f93ad99bc4ecf92f1db385f73184ec551dc8b
attilka19 <31649896+atilka19@users.noreply.github.com>
2018-05-30 19:04:23 +0200

    Location chooser done

--- src/shoreline/GUI/Controller/Model.java
+++ src/shoreline/GUI/Controller/Model.java
@@ -7,10 +7,18 @@ package shoreline.GUI.Controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import shoreline.BE.JSonObject;
 
 /**
@@ -24,22 +32,45 @@ public class Model {
     //FileWriter method. Used to create the output JSon file "testfile.json.
     //TODO create filesaver dialogue for dir choosing UX.
     public void convert(ObservableList<JSonObject> listData) throws IOException{
-         try (FileWriter file = new FileWriter("testfile.json")) {
-             
-
-               Gson gson = new GsonBuilder().setPrettyPrinting().create();
-               String json = gson.toJson(listData);
-               
-               
-               
-                    
-               System.out.println(json);
-               file.write(json);
-               file.flush();
-
-
-              listOfJasons.add(json);
-        }
+         try (FileWriter file = new FileWriter("testfile.json")) 
+         {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(listData);
+            Platform.runLater(new Runnable() 
+            {
+                public void run() 
+                {
+                    Stage stage2 = new Stage();
+                    FileChooser fc = new FileChooser();
+                    FileChooser.ExtensionFilter jsonFilter = new 
+                    FileChooser.ExtensionFilter("Json files (*.json)", "*.json");
+                    fc.getExtensionFilters().add(jsonFilter);
+                    File savepath = fc.showSaveDialog(stage2);
+                    if (file != null) 
+                    {
+                        try 
+                        {
+                            saveTextToFile(json, savepath);
+                        } 
+                        catch (FileNotFoundException ex) 
+                        {
+                            Logger.getLogger(Model.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+            );
+            listOfJasons.add(json);
+         }
     }
+    
+    private void saveTextToFile(String content, File file) throws FileNotFoundException
+    {
+        PrintWriter writer;
+        writer = new PrintWriter(file);
+        writer.println(content);
+        writer.close();
+    } 
+
     
 }
